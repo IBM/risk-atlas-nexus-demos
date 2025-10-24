901f07c81a4c16659825339b0fff385780c34efe
INGE VEJSBJERG <INGEVEJS@ie.ibm.com>
2025-10-24 16:46:06 +0100

    chore: update risk content and styles
    Signed-off-by: INGE VEJSBJERG <INGEVEJS@ie.ibm.com>

--- ran-viz/front-end/demo/src/index.tsx
+++ ran-viz/front-end/demo/src/index.tsx
@@ -1,7 +1,7 @@
 
 import ReactDOM from "react-dom/client";
 
-import "./styles.scss";
+import "./styles.css";
 import Root from "./views/Root";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
